a22aeb2a1e98921a6faf6e90cb8f95a28c69c601
Egor Kosolapov <greezzly7@gmail.com>
2014-04-09 16:55:32 +0400

    second commit

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function(){
-// 	// var ww1 = $(window).width();
-// 		$('#map ymaps').css({'width':'100%'});
-// 	$(window).resize(function(){
-// 	 	var ww = $(window).width();
-// 		$('#map ymaps').css({'width':ww});
-// 	});
-// });
